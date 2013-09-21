330751ec22ad21a9bf014e3ce6cce37e198e5e42
Todd Pickell <myappleguy@gmail.com>
2013-09-21 11:00:33 -0500

    getting task class filled out tdd styley

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,5 +1,18 @@
 class Task
+  attr_reader :name, :subtasks
+  def initialize(name)
+    @name = name
+    @subtasks = []
+    @completed = false
+  end
 
+  def completed?
+    @completed
+  end
+
+  def <<(task)
+    @subtasks << task
+  end
 end
 
 class Visitor
--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -1,7 +1,30 @@
 require 'visitor'
 
 describe Task do
+  before(:each) do
+    @task = Task.new('Task One')
+  end
 
+  describe 'initilization' do
+    it 'initializes with a name' do
+      @task.name.should == 'Task One'
+    end
+
+    it 'initializes with an empty set of subtasks' do
+      @task.subtasks.should == []
+    end
+
+    it 'initializes not completed' do
+      @task.completed?.should == false
+    end
+  end
+
+  describe 'adding subtasks to parent task' do
+    it 'adds tasks to the subtasks array' do
+      @task << Task.new('Subtask One')
+      @task.subtasks.first.name.should == 'Subtask One'
+    end
+  end
 end
 
 describe Visitor do
